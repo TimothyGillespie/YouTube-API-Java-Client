03c2972fbcbfac6fee17fd2103e07d7a241d525d
Timothy Gillespie <Timothy.Gillespie@outlook.de>
2021-03-15 15:49:01 +0100

    Add error handling

--- src/main/java/eu/gillespie/youtubeapi/YouTubeApiV3.java
+++ src/main/java/eu/gillespie/youtubeapi/YouTubeApiV3.java
@@ -1,14 +1,20 @@
 package eu.gillespie.youtubeapi;
 
 import eu.gillespie.youtubeapi.model.PaginatedActivity;
+import lombok.AccessLevel;
+import lombok.Getter;
 import org.javawebstack.httpclient.HTTPClient;
+import org.javawebstack.httpclient.HTTPRequest;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class YouTubeApiV3 {
 
+    @Getter(AccessLevel.NONE)
     private String apiKey;
+
+    @Getter
     private int quotaCostIncurred;
 
     private static final String API_BASE_URL = "https://www.googleapis.com/youtube/v3";
@@ -20,13 +26,17 @@ public class YouTubeApiV3 {
     }
 
     public PaginatedActivity getActivitiesByChannelId(String channelId) {
-        return this.client
+        HTTPRequest response = this.client
                 .get("/activities")
                 .query("key", this.apiKey)
                 .query("channelId", channelId)
                 .query("part", "snippet")
-                .execute()
-                .object(PaginatedActivity.class);
+                .execute();
+
+        if (!handleErrorCases(response))
+            return null;
+
+        return response.object(PaginatedActivity.class);
     }
 
     public int addQuotaCost(int cost) {
@@ -36,6 +46,21 @@ public class YouTubeApiV3 {
                 () -> String.format("Quota cost of %d has been added. New total: %d", cost, this.quotaCostIncurred)
         );
         return this.quotaCostIncurred;
+    }
+
+    private boolean handleErrorCases(HTTPRequest request) {
+
+        if (!(request.status() >= 200 && request.status() < 200)) {
+            Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).log(
+                    Level.SEVERE,
+                    "Request was unsuccessful: {}",
+                    request
+            );
+
+            return false;
+        }
+
+        return true;
     }
 
 }
